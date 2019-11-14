59d441374d25c2cf0b2fc8055c63f5a6b4efc136
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2019-11-14 11:29:47 -0500

    Updates

---
+++ precache-manifest.c54bd68087c37c617ce1f9cd7b1c4c72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40946e29c6a10f46a9a074251b88d3c8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ba7b0771c6e4cedf22f4",
+    "url": "/monsters-rolodex/static/css/main.312409f1.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/monsters-rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "ba7b0771c6e4cedf22f4",
+    "url": "/monsters-rolodex/static/js/main.da77e838.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
